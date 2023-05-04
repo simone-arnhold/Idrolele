56c6dea1832cfc5730d9fece9a762c03716bce01
Simone Arnhold <simone.arnhold@outlook.com>
2023-05-04 17:21:23 +0200

    make testimonial section

--- scripts/servizi.js
+++ scripts/servizi.js
@@ -40,71 +40,111 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     //         console.log("vecchio i:", vecchioIndex)
 
-    //         descrizioniServizi.forEach(element => {
-    //             element.classList.remove("selected")
-    //             blocchi.forEach(element => {
-    //                 element.classList.remove("selected")
-    //             })
-    //         })
+    // descrizioniServizi.forEach(element => {
+    //     element.classList.remove("selected")
+    //     blocchi.forEach(element => {
+    //         element.classList.remove("selected")
+    //     })
+    // })
     //         nuovoIndex = Array.from(blocchi).indexOf(elemento)
     //         elemento.classList.add("selected")
     //         descrizioniServizi[index].classList.add("selected")
     //         console.log("current index: ", index)
     //     }
     // })
-    // vecchia versione
+
+    // // vecchia versione
+    // blocchi.forEach(element => {
+    //     element.addEventListener("mouseover", () => {
+    //         // remove all hover tags
+    //         descrizioniServizi.forEach(element => {
+    //             element.classList.remove("selected")
+    //             blocchi.forEach(element => {
+    //                 element.classList.remove("selected")
+    //             })
+    //         })
+    //         index = Array.from(blocchi).indexOf(element)
+    //         element.classList.add("selected")
+    //         descrizioniServizi[index].classList.add("selected")
+    //         console.log("current index: ", index)
+    //         // order:
+    //         // #pennello
+    //         // #martello
+    //         // #chiaveingl
+    //         // #lavandino
+    //         // #stur
+    //         // #cazzuola
+    //         // #pappagallo
+    //     })
+    // })
+
+    // hover
     blocchi.forEach(element => {
         element.addEventListener("mouseover", () => {
-            // remove all hover tags
-            descrizioniServizi.forEach(element => {
-                element.classList.remove("selected")
-                blocchi.forEach(element => {
-                    element.classList.remove("selected")
-                })
-            })
+            rimuoviSelected()
             index = Array.from(blocchi).indexOf(element)
             element.classList.add("selected")
             descrizioniServizi[index].classList.add("selected")
             console.log("current index: ", index)
-            // order:
-            // #pennello
-            // #martello
-            // #chiaveingl
-            // #lavandino
-            // #stur
-            // #cazzuola
-            // #pappagallo
+        });
+
+        // rimuovi tutti i tags selected
+        function rimuoviSelected() {
+            descrizioniServizi.forEach(element => {
+                element.classList.remove("selected")
+            })
+            blocchi.forEach(element => {
+                element.classList.remove("selected")
+            })
+        }
+
+        function changeSlide(index, hoveredElement) {
+            rimuoviSelected()
+
+            // logic on buttons
+
+
+            // logic on hover
+            if (hoveredElement != undefined) {
+                hoveredElement.classList.add("selected")
+                descrizioniServizi[index].classList.add("selected")
+                console.log("current index: ", index)
+            }
+
+
+        }
+
+        // carets navigation
+        const navCaretLeft = document.querySelector(".fa-caret-left")
+        const navCaretRight = document.querySelector(".fa-caret-right")
+        console.log(navCaretRight)
+
+        navCaretLeft.addEventListener("click", () => {
+            console.log("oldindex ", index)
+            index -= 1
+            // if (index < 0) {
+            //     index = 6
+            // }
+            console.log("clicked caret left, newindex: ", index)
         })
-    })
 
+        navCaretRight.addEventListener("click", () => {
+            index += 1
+            // if (index > 6) {
+            //     index = 0
+            // }
+            console.log("clicked caret right, newindex: ", index)
+        })
 
-    // carets navigation
-    const navCaretLeft = document.querySelector(".fa-caret-left")
-    const navCaretRight = document.querySelector(".fa-caret-right")
-
-    navCaretLeft.addEventListener("click", () => {
-        index -= 1
-        if (index < 0) {
-            index = 6
+        // adattamento viewbox mobile
+        const svgLogo = document.getElementById("Livello_1");
+        function fixSvgMobile(svgLogo) {
+            if (window.innerWidth < 768) {
+                // original viewBox: 0 0 820.2 1197.2
+                svgLogo.setAttribute("viewBox", "0 0 580 1197.2");
+            }
         }
+        fixSvgMobile(svgLogo)
+        addEventListener("resize", fixSvgMobile(svgLogo))
     })
-
-    navCaretRight.addEventListener("click", () => {
-        index += 1
-        console.log(index)
-        if (index > 6) {
-            index = 0
-        }
-    })
-
-    // adattamento viewbox mobile
-    const svgLogo = document.getElementById("Livello_1");
-    function fixSvgMobile(svgLogo) {
-        if (window.innerWidth < 768) {
-            // original viewBox: 0 0 820.2 1197.2
-            svgLogo.setAttribute("viewBox", "0 0 580 1197.2");
-        }
-    }
-    fixSvgMobile(svgLogo)
-    addEventListener("resize", fixSvgMobile(svgLogo))
 })
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,7 +1,7 @@
-const splide = new Splide(".splide", {
+const heroSplide = new Splide("#hero__splide", {
     // inView: autoplay,
     // start: 0,
-    // perPage: 2,
+    perPage: 3,
     perMove: 1,
     gap: 20,
     type: "loop",
@@ -29,4 +29,41 @@ const splide = new Splide(".splide", {
     }
 });
 
-splide.mount();
+heroSplide.mount();
+
+const testimonialSplide = new Splide("#testimonial__splide", {
+    // inView: autoplay,
+    // start: 0,
+    // perPage: 4,
+    perMove: 1,
+    gap: 50,
+    type: "loop",
+    autoWidth: true,
+    // autoplay: true,
+    // pauseOnHover: false,
+    // pauseOnFocus: true,
+    // resetProgress: true,
+    // interval: 15000,
+    drag: "free",
+    snap: false,
+    arrows: true,
+    pagination: true,
+    rewind: true,
+    rewindByDrag: true,
+    lazyLoad: true,
+    padding: "5rem",
+    // width: "100vw",
+
+
+    // Responsive breakpoint
+    breakpoints: {
+        768: {
+            perPage: 1,
+            snap: true,
+            // width: "100vw",
+            gap: 20,
+        }
+    }
+});
+
+testimonialSplide.mount()
