89660a201de09e6a818fabf2c7b63d4ec9a87683
Simone Arnhold <simone.arnhold@outlook.com>
2023-07-18 15:16:54 +0200

    fix style

--- scripts/slider-portfolio.js
+++ scripts/slider-portfolio.js
@@ -1,47 +1,47 @@
 // portfolio splide
 const portfolioSplide = new Splide("#portfolio__splide", {
-    // inView: autoplay,
-    start: 0,
-    perPage: 4,
-    perMove: 1,
-    gap: 30,
-    type: "loop",
-    // pauseOnHover: false,
-    // pauseOnFocus: true,
-    // resetProgress: true,
-    // interval: 15000,
-    drag: "free",
-    snap: false,
-    // arrows: true,
-    // pagination: true,
-    // rewind: true,
-    // rewindByDrag: true,
-    lazyLoad: true,
-    width: "80vw",
-    // padding: "5rem",
-    // autoWidth: true,
-    // Responsive breakpoint
+    //     // inView: autoplay,
+    //     start: 0,
+    //     perPage: 4,
+    //     perMove: 1,
+    //     gap: 30,
+    //     type: "loop",
+    //     // pauseOnHover: false,
+    //     // pauseOnFocus: true,
+    //     // resetProgress: true,
+    //     // interval: 15000,
+    //     drag: "free",
+    //     snap: false,
+    //     // arrows: true,
+    //     // pagination: true,
+    //     // rewind: true,
+    //     // rewindByDrag: true,
+    //     lazyLoad: true,
+    //     width: "80vw",
+    //     // padding: "5rem",
+    //     // autoWidth: true,
+    //     // Responsive breakpoint
 
 
 
-    breakpoints: {
-        1280: {
-            perPage: 1,
-        },
+    //     breakpoints: {
+    //         1280: {
+    //             perPage: 1,
+    //         },
 
-        768: {
-            // type: "fade",
-            perPage: 1,
-            gap: 10,
-            snap: true,
-            // width: "80vw",
-            // height: "500px",
-            focus: "center",
-            // heightRatio: 0.5,
-            // autoHeight: true,
-            // autoWidth: true,
-        }
-    }
+    //         768: {
+    //             // type: "fade",
+    //             perPage: 1,
+    //             gap: 10,
+    //             snap: true,
+    //             // width: "80vw",
+    //             // height: "500px",
+    //             focus: "center",
+    //             // heightRatio: 0.5,
+    //             // autoHeight: true,
+    //             // autoWidth: true,
+    //         }
+    //     }
 });
 
 portfolioSplide.mount();
